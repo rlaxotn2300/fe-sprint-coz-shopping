87e1125bddc07611d0f3fa78e4641b811d4f3295
rlaxotn2300 <rlaxotn2300@naver.com>
2023-07-14 16:40:43 +0900

    데이터 불러오기 완료

--- src/Page/Section.js
+++ src/Page/Section.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import './Section.css'
+import './Section.css';
 
-const Section = () => {
+const Section = ({ isData }) => {
+
+    const filter_product =
+        isData ? isData.filter(obj => {
+            return obj.type === 'Product'
+        }) : console.log('noData');
+
+    const filter_bookMark =
+        isData ? isData.filter(obj => {
+            return obj.type === 'Category'
+        }) : console.log('noData')
+
     return (
         <div className='section'>
             <div className='product_list'>
                 <h2>상품 리스트</h2>
+                <ul>
+                    {filter_product ? filter_product.map((obj, idx) => {
+                        if(idx < 4){return <li>
+                            <img src={obj.image_url} />
+                            <p className='h3'><strong>{obj.title}</strong></p>
+                            <p>{`${obj.price}원`}</p>
+                        </li>
+                    }}) : console.log('null')}
+                </ul>
             </div>
             <div className='bookmark_list'>
                 <h2>북마크 리스트</h2>
+                <ul>
+                    {filter_product ? filter_bookMark.map((obj, idx) => {
+                        if(idx < 4){return <li>
+                            <img src={obj.image_url} />
+                            <p className='h3'><strong>{obj.title}</strong></p>
+                        </li>
+                    }}) : console.log('null')}
+                </ul>
             </div>
         </div>
     )
 }
 
 export default Section
+
+{/* <img src={isData ? isData[0].image_url : ''} /> */ }
